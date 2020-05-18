e056c2729f8b96bc1dcf5b6d568a0e4034564d00
Jakob Vesely <jakob@appartig.at>
2020-05-18 21:22:12 +0200

    settings page

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { SettingsService } from 'src/app/services/settings/settings.service';
 
 @Component({
     selector: "app-settings",
@@ -7,7 +8,7 @@ import { Component, OnInit } from "@angular/core";
 })
 export class SettingsPage implements OnInit
 {
-    constructor() { }
+    constructor(private settings: SettingsService) { }
 
     ngOnInit() { }
 }
--- src/app/services/settings/settings.service.ts
+++ src/app/services/settings/settings.service.ts
@@ -16,50 +16,58 @@ export class SettingsService
                 sortIndex: 10,
                 name: "Leinen anlegen",
                 trainingTypes: {
-                    kuppeln: true,
-                    gesamt: true,
-                },
+                    kuppeln: {
+                        available: true,
+                        setting: true
+                    },
+                    gesamt: {
+                        available: true,
+                        setting: true
+                    }
+                }
             },
             {
                 sortIndex: 20,
                 name: "Saugleitung zu Wasser",
                 trainingTypes: {
-                    kuppeln: true,
-                    gesamt: true,
-                },
+                    kuppeln: {
+                        available: true,
+                        setting: true
+                    },
+                    gesamt: {
+                        available: true,
+                        setting: true
+                    }
+                }
             },
             {
                 sortIndex: 30,
                 name: "Angesaugt",
                 trainingTypes: {
-                    kuppeln: true,
-                    gesamt: true,
-                },
+                    kuppeln: {
+                        available: true,
+                        setting: true
+                    },
+                    gesamt: {
+                        available: true,
+                        setting: true
+                    }
+                }
             },
             {
                 sortIndex: 40,
                 name: "STF Wasser marsch",
                 trainingTypes: {
-                    kuppeln: false,
-                    gesamt: true,
-                },
-            },
-            {
-                sortIndex: 50,
-                name: "STF Wasser marsch",
-                trainingTypes: {
-                    kuppeln: false,
-                    gesamt: true,
-                },
-            },
-            {
-                sortIndex: 60,
-                name: "STF Wasser marsch",
-                trainingTypes: {
-                    kuppeln: false,
-                    gesamt: true,
-                },
-            },
+                    kuppeln: {
+                        available: false,
+                        setting: false
+                    },
+                    gesamt: {
+                        available: true,
+                        setting: true
+                    }
+                }
+            }
         ];
     }
 
